b7bdd669d4d9b9a434f97bfd69e98d622d1810b2
vishalpalle <pallevishal2@gmail.com>
2025-05-14 00:49:33 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages,setup
+from typing import List
+Hyphen_dot_e = "-e ."
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        reuirements = file_obj.readlines()
+        requirements= [req.replace ("\n" , " ")for req in requirements]
+
+        if Hyphen_dot_e in requirements:
+            requirements.remove(Hyphen_dot_e)
+
+setup(
+    name ='mlproject',
+    version = '0.0.1',
+    author='Vishal',
+    author_email='pallevishal2@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
